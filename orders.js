c46282111042681f93dd16c3c8247169f3d7a51e
Ehindola Odunayo <45654843+ehindola@users.noreply.github.com>
2022-08-12 03:09:40 +0100

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+        productName: 'Foldable mini Drone',
+        productNumber: '87562',
+        paymentStatus: 'Due',
+        Shipping: 'Pending'
+    },
+    {
+        productName: 'Sweet Love Drone',
+        productNumber: '37486',
+        paymentStatus: 'Refunded',
+        Shipping: 'Declined'
+    },
+    {
+        productName: 'Excel Pro Drone',
+        productNumber: '27562',
+        paymentStatus: 'Due',
+        Shipping: 'Pending'
+    },
+    {
+        productName: 'Hawk F12 Drone',
+        productNumber: '62462',
+        paymentStatus: 'Paid',
+        Shipping: 'Delivered'
+    },
+    {
+        productName: 'GLK 450 Drone',
+        productNumber: '15590',
+        paymentStatus: 'Paid',
+        Shipping: 'Delivered'
+    },
+]
